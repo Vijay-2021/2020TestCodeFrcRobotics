b29939a4490b0a1e21691f20441fd319ad326b7e
Vijay-2021 <42916674+Vijay-2021@users.noreply.github.com>
2020-01-11 15:44:22 -0500

    Update RobotContainer.java

--- frc/robot/RobotContainer.java
+++ frc/robot/RobotContainer.java
@@ -8,12 +8,16 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.LimeLight;
+
 import frc.robot.commands.PathFollower;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -26,7 +30,8 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final DriveTrain trains = new DriveTrain();
   private final PathFollower follow10 = new PathFollower(trains);
-
+  Joystick left ;
+  JoystickButton leftTrigger; 
 
 
 
@@ -34,6 +39,8 @@ public class RobotContainer {
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
+    left = new Joystick(0);
+    leftTrigger = new JoystickButton(left,1);
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -45,6 +52,7 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
+    leftTrigger.whenPressed(new LimeLight(trains));
   }
 
 
@@ -57,4 +65,5 @@ public class RobotContainer {
     // An ExampleCommand will run in autonomous
     return follow10;
   }
+
 }
